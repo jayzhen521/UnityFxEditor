79e88eb4534a1917440dec879b2cfd967d91f210
Helin777 <19297905+Helin777@users.noreply.github.com>
2021-03-24 20:35:56 +0800

    为抖动添加了更多的细节控制

--- runtime/AnimationTools/JitterAnimation.cs
+++ runtime/AnimationTools/JitterAnimation.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using Quaternion = System.Numerics.Quaternion;
 using Random = System.Random;
 using Vector3 = UnityEngine.Vector3;
 
@@ -9,14 +11,22 @@ namespace Packages.FxEditor
     public class JitterAnimation : MonoBehaviour
     {
         public int frequency = 1;
-        public float power = 0.1f;
+        public float powerT = 0.1f;
+        public float powerR = 0.1f;
+        public float powerS = 0.1f;
+        public int seed = DateTime.Now.Millisecond;
 
         private int count = 0;
         private Vector3 pos = Vector3.zero;
-        private Random rnd = new Random();
+        private Vector3 sc = Vector3.zero;
+        private UnityEngine.Quaternion rot=UnityEngine.Quaternion.identity;
+        private Random rnd = null;
         private void Start()
         {
+            rot = transform.rotation;
+            rnd = new Random(seed);
             pos = transform.position;
+            sc = transform.localScale;
             count = 0;
         }
 
@@ -25,9 +35,18 @@ namespace Packages.FxEditor
             count++;
             if ((count % frequency) == 0)
             {
-                float x = (float)rnd.NextDouble() * power-power*.5f;
-                float y = (float)rnd.NextDouble() * power-power*.5f;
+                float x = (float)rnd.NextDouble() * powerT-powerT*.5f;
+                float y = (float)rnd.NextDouble() * powerT-powerT*.5f;
                 transform.position = pos + (new Vector3(x, y, 0));
+                
+                 x = (float)rnd.NextDouble() * powerS-powerS*.5f;
+                 y = (float)rnd.NextDouble() * powerS-powerS*.5f;
+                 transform.localScale = sc + (new Vector3(x, x, 0));
+                 
+                 
+                 x = (float)rnd.NextDouble() * powerR-powerR*.5f;
+                 UnityEngine.Quaternion q = UnityEngine.Quaternion.AngleAxis(x, Vector3.forward);
+                 transform.rotation =rot*q;
             }
         }
     }
