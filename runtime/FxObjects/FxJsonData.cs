fd0c672e1def4d48558e3260eeae74e06c0faa15
Helin <helin777@outlook.com>
2021-07-02 15:38:54 +0800

    fix

--- runtime/FxObjects/FxJsonData.cs
+++ runtime/FxObjects/FxJsonData.cs
@@ -19,48 +19,57 @@ namespace Packages.FxEditor
     public class EffectItem
     {
         public int duration = 0;
-        public int start_time=0;
-        public int end_time=0;
+        public int start_time = 0;
+        public int end_time = 0;
         public int edit_filter_id1 = -1;
         public int edit_filter_id2 = -1;
         public int type = -2;
         public string path = "00";
         //----------------------
+        public int opacityID = 0;
     }
     [Serializable]
-    public class EffectItemFeatures{
-        public int none=0;
-        public int cutout;
-        public int cartoon;
+    public class EffectItemFeatures
+    {
+        public int itemID = 0;
+        public effectFeaturesListTypes type = effectFeaturesListTypes.none; // 
+        public Boolean needPic = false;
     }
-
+    public enum effectFeaturesListTypes
+    {
+        none = 0,
+        cutout
+    }
     public enum PlayMode
     {
         Once,//===0
         Loop,//===1
         PingPong
     }
-    
- public enum supportSizesTypes
+
+    public enum supportSizesTypes
     {
-        
-        A_1x2=1,//===1
+
+        A_16x9 = 1,//===1
+        A_4x3,
+        A_1x1,
         A_3x4,
-        A_4x3
+        A_9x16
     }
 
-    public class FxJsonData : FxJsonDataBase
+    public class FxJsonData : MonoBehaviour
     {
         //-----------Json数据--------------
+        public int filterId = -1;
         public string name = "";
         public float totalDuration = 0.0f;
         public PlayMode play_mode = PlayMode.Once;
-        public List<supportSizesTypes> supportSizes=new List<supportSizesTypes>();
+        public List<supportSizesTypes> supportSizes = new List<supportSizesTypes>();
         public List<EffectItem> effectList = new List<EffectItem>();
 
 
-     
-        
+
+
         public string musicConfig = "999";
         public int isTransRand = 0;
         public int backgroundColor = 3;
@@ -73,45 +82,49 @@ namespace Packages.FxEditor
         public List<int> clip_duration = new List<int>();
 
 
-        public List<EffectItemFeatures> effectFeaturesList=new List<EffectItemFeatures>(); 
-        
+        public List<EffectItemFeatures> effectFeaturesList = new List<EffectItemFeatures>();
+
         [Header("用户自定义数据")]
         //--------------------------------------------
         public string userdata = "999";
         public int userInteger = 9999;
         public float userFloat = 9999.0f;
-        public Boolean userBoolean =true;
+        public Boolean userBoolean = true;
         //--------------------------------------------
 
 
-        
+        public void SaveTo(string filepath)
+        {
+            var text = JsonUtility.ToJson(this);
+            File.WriteAllText(filepath, text);
+        }
 
 
         void UpdateTimelineData()
         {
             clip_duration.Clear();
-            
+
             var timeline = UnityEngine.Object.FindObjectOfType<Timeline>();
             if (timeline == null) return;
 
-    
 
-             effectList.Clear();
-     
-            
+
+            effectList.Clear();
+
+
             float starttime = 0;
             totalDuration = 0;
             for (int i = 0; i < timeline.clips.Count; i++)
             {
                 var clip = timeline.clips[i];
-                
-                var fx=new EffectItem();
-                
+
+                var fx = new EffectItem();
+
                 //fx.path = string.Format("{0:00}",i);
-                fx.duration = (int) (clip.duration * 1000);
-                fx.start_time=(int)(starttime*1000);
-                fx.end_time=(int)((starttime+clip.duration)*1000);
-                fx.type = (int) clip.type;
+                fx.duration = (int)(clip.duration * 1000);
+                fx.start_time = (int)(starttime * 1000);
+                fx.end_time = (int)((starttime + clip.duration) * 1000);
+                fx.type = (int)clip.type;
                 effectList.Add(fx);
                 starttime += clip.duration;
 
@@ -121,12 +134,14 @@ namespace Packages.FxEditor
                     clip_duration.Add(fx.duration);
                     totalDuration += fx.duration;
                 }
+
             }
 
             clipNum = clip_duration.Count;
         }
 
-    
+
+
         private void OnDrawGizmos()
         {
             UpdateTimelineData();
